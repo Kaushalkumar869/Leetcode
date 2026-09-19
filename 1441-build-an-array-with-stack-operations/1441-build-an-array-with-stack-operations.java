3ef23add9b4ce84ed0474e8d7094c3b34d7cdb3f
Kaushal Kumar <kaushal706166@gmail.com>
2026-09-19 15:23:44 +0530

    Time: 0 ms (100%), Space: 44.3 MB (52%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        Stack<String> ss = new Stack< >();
+        int i = 1;
+        int j = 0;
+        while(j<target.length){
+            if(i==target[j]){
+                ss.push("Push");
+                j++;
+                i++;
+            }
+            else{
+                ss.push("Push");
+                ss.push("Pop");
+                i++;
+            }
+        }
+        return ss;
+    }
+}
